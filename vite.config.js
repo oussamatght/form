e166a5c08647a3d5fcc6e0c0dc59730764f4871d
oussama <oussamatght6@gmail.com>
2025-10-08 15:48:13 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: './',
   preview: {
     allowedHosts: ['formmm-sqzj.onrender.com'],
-    host: '0.0.0.0',
-    port: process.env.PORT || 5173,
-  },
-  server: {
     host: '0.0.0.0',
     port: process.env.PORT || 5173,
   },
