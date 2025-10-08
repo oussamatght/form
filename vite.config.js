70ccfca0e79370958f5894a67c0cd1ffeae4a66f
oussama <oussamatght6@gmail.com>
2025-10-08 15:39:33 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  preview: {
+    allowedHosts: ['formmm-sqzj.onrender.com'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
+  },
 })
